158a67d7f01f9b632a57a20c0a7d0ba8943a2350
fairoos <fairoosnm123@gmail.com>
2019-02-11 20:02:20 +0530

    Red: Adds add_test_for_win

--- test_hangman.py
+++ test_hangman.py
@@ -25,4 +25,40 @@ def test_chek_gussed_char_psition():
     assert (hangman.chek_gussed_char('a', 'mankind')) == [1]
 
 def test_add_gussed_char_masked_word():
-    assert(hangman.add_gussed_char_masked_word([2,5], 'n','******')) == '**n**n*' 
+    assert(hangman.add_gussed_char_masked_word([1,5,6], 'n','********')) == ['*', 'n', '*', '*', '*', 'n', 'n', '*']
+
+
+def test_for_Win_main():
+    input_values=['e','l', 'p','h','a','n','t']
+    output=[]
+
+    def mock_input(s):
+        output.append(s)
+        return input_values.pop(0)
+
+    hangman.input=mock_input
+    hangman.print=lambda s :output.append(s)
+
+    hangman.main("elephant")
+            
+    assert output[1] == "Number of tries left: 10"
+    assert output[3] == "Enter your guess:  "
+    assert output[4] == "e*e*****"
+    assert output[35] == "Congratulations! You WON."
+def test_for_Lose_main():
+
+    input_values=['k','k', 'k','k','k','k','k','k','k','k']
+    output=[]
+
+    def mock_input(s):
+        output.append(s)
+        return input_values.pop(0)
+
+    hangman.input=mock_input
+    hangman.print=lambda s :output.append(s)
+
+    hangman.main("elephant")
+
+    assert output[1] == "Number of tries left: 10"
+    assert output[4] == "Wrong Guess:"
+    assert output[50] == "Too bad! The secret word was elephant"
